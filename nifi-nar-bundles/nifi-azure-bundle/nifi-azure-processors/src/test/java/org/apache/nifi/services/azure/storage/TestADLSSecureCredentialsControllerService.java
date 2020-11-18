18f58664c5560ed4d2ee4d17888a3e03eceb96a6
Sushil Kumar <suskuma@microsoft.com>
2020-11-18 13:50:03 -0800

    Adds secure controller service using keyvault secrets and tests
    Adds following secure controller services using credentials from keyvault secret
    - AzureKeyVaultClientService
    - AzureStorageSecureCredentialsControllerService
    - ADLSSecureCredentialsControllerService
    - AzureCosmosSecureDBClientService
    
    Added tests for
    - AzureKeyVaultUtils
    - AzureKeyVaultClientService
    - ADLSSecureCredentialsControllerService
    - AzureStorageSecureCredentialsControllerService
    - AzureCosmosSecureDBClientService

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/services/azure/storage/TestADLSSecureCredentialsControllerService.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/services/azure/storage/TestADLSSecureCredentialsControllerService.java
@@ -16,38 +16,83 @@
  */
 package org.apache.nifi.services.azure.storage;
 
+import java.util.HashMap;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+
 import org.apache.nifi.processors.azure.storage.utils.AzureStorageUtils;
 import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.services.azure.keyvault.AzureKeyVaultClientService;
 import org.apache.nifi.util.NoOpProcessor;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
-public class TestADLSCredentialsControllerService {
+class MockKeyVaultConnectionService extends AzureKeyVaultClientService {
 
-    public static final String CREDENTIALS_SERVICE_IDENTIFIER = "credentials-service";
+    private static final String ACCOUNT_NAME_SECRET_NAME = "AccountNameSecret";
+    private static final String ACCOUNT_KEY_SECRET_NAME = "AccountKeySecret";
+    private static final String SAS_TOKEN_SECRET_NAME = "SasTokenSecret";
+    private static final String END_POINT_SUFFIX_SECRET_NAME = "end.point.suffix";
 
-    private static final String ACCOUNT_NAME_VALUE = "AccountName";
-    private static final String ACCOUNT_KEY_VALUE = "AccountKey";
-    private static final String SAS_TOKEN_VALUE = "SasToken";
-    public static final String END_POINT_SUFFIX_VALUE = "end.point.suffix";
+    private String ACCOUNT_NAME_SECRET_VALUE = "AccountNameSecretValue";
+    private String ACCOUNT_KEY_SECRET_VALUE = "AccountKeySecretValue";
+    private String SAS_TOKEN_SECRET_VALUE = "SasTokenSecretValue";
+    private String END_POINT_SUFFIX_SECRET_VALUE = "end.point.suffix";
+
+    private String MOCK_INVALID_SECRET_VALUE = "INVALID_VALUE";
+
+    @Override
+    public SecretClient getKeyVaultSecretClient(){
+        return mock(SecretClient.class);
+    }
+
+    @Override
+    public String getSecretFromKeyVault(String secretName) {
+        switch(secretName) {
+            case ACCOUNT_NAME_SECRET_NAME:
+                return ACCOUNT_NAME_SECRET_VALUE;
+            case ACCOUNT_KEY_SECRET_NAME:
+                return ACCOUNT_KEY_SECRET_VALUE;
+            case SAS_TOKEN_SECRET_NAME:
+                return SAS_TOKEN_SECRET_VALUE;
+            case END_POINT_SUFFIX_SECRET_NAME:
+                return END_POINT_SUFFIX_SECRET_VALUE;
+            default:
+               return MOCK_INVALID_SECRET_VALUE;
+        }
+    }
+}
+
+public class TestADLSSecureCredentialsControllerService {
+
+    public static final String CREDENTIALS_SERVICE_IDENTIFIER = "secure-credentials-service";
+
+    private static final String ACCOUNT_NAME_SECRET = "AccountNameSecret";
+    private static final String ACCOUNT_KEY_SECRET = "AccountKeySecret";
+    private static final String SAS_TOKEN_SECRET = "SasTokenSecret";
+    private static final String END_POINT_SUFFIX_SECRET = "end.point.suffix";
+
+    private String ACCOUNT_NAME_SECRET_VALUE = "AccountNameSecretValue";
+    private String ACCOUNT_KEY_SECRET_VALUE = "AccountKeySecretValue";
+    private String SAS_TOKEN_SECRET_VALUE = "SasTokenSecretValue";
+    private String END_POINT_SUFFIX_SECRET_VALUE = "end.point.suffix";
 
     private TestRunner runner;
-    private ADLSCredentialsControllerService credentialsService;
+    private ADLSSecureCredentialsControllerService credentialsService;
 
     @Before
     public void setUp() throws InitializationException {
         runner = TestRunners.newTestRunner(NoOpProcessor.class);
-        credentialsService = new ADLSCredentialsControllerService();
+        credentialsService = new ADLSSecureCredentialsControllerService();
         runner.addControllerService(CREDENTIALS_SERVICE_IDENTIFIER, credentialsService);
     }
 
@@ -68,40 +113,8 @@ public class TestADLSCredentialsControllerService {
     @Test
     public void testNotValidBecauseBothAccountKeyAndSasTokenSpecified() {
         configureAccountName();
-
         configureAccountKey();
         configureSasToken();
-
-        runner.assertNotValid(credentialsService);
-    }
-
-    @Test
-    public void testNotValidBecauseBothAccountKeyAndUseManagedIdentitySpecified() {
-        configureAccountName();
-
-        configureAccountKey();
-        configureUseManagedIdentity();
-
-        runner.assertNotValid(credentialsService);
-    }
-
-    @Test
-    public void testNotValidBecauseBothSasTokenAndUseManagedIdentitySpecified() {
-        configureAccountName();
-
-        configureSasToken();
-        configureUseManagedIdentity();
-
-        runner.assertNotValid(credentialsService);
-    }
-
-    @Test
-    public void testNotValidBecauseAllCredentialsSpecified() {
-        configureAccountName();
-
-        configureAccountKey();
-        configureSasToken();
-        configureUseManagedIdentity();
 
         runner.assertNotValid(credentialsService);
     }
@@ -114,35 +127,30 @@ public class TestADLSCredentialsControllerService {
         runner.setProperty(credentialsService, AzureStorageUtils.ADLS_ENDPOINT_SUFFIX, "");
         runner.assertNotValid(credentialsService);
     }
+
     @Test
     public void testNotValidWithWhitespaceEndpointSuffix() {
         configureAccountName();
         configureAccountKey();
-
         runner.setProperty(credentialsService, AzureStorageUtils.ADLS_ENDPOINT_SUFFIX, " ");
+
         runner.assertNotValid(credentialsService);
     }
 
     @Test
-    public void testValidWithAccountNameAndAccountKey() {
+    public void testValidWithAccountNameAndAccountKey() throws InitializationException {
         configureAccountName();
         configureAccountKey();
+        configureKeyVaultConnectionService();
 
         runner.assertValid(credentialsService);
     }
 
     @Test
-    public void testValidWithAccountNameAndSasToken() {
+    public void testValidWithAccountNameAndSasToken() throws InitializationException {
         configureAccountName();
         configureSasToken();
-
-        runner.assertValid(credentialsService);
-    }
-
-    @Test
-    public void testValidWithAccountNameAndUseManagedIdentity() {
-        configureAccountName();
-        configureUseManagedIdentity();
+        configureKeyVaultConnectionService();
 
         runner.assertValid(credentialsService);
     }
@@ -152,6 +160,7 @@ public class TestADLSCredentialsControllerService {
         // GIVEN
         configureAccountName();
         configureAccountKey();
+        configureKeyVaultConnectionService();
 
         runner.enableControllerService(credentialsService);
 
@@ -159,8 +168,8 @@ public class TestADLSCredentialsControllerService {
         ADLSCredentialsDetails actual = credentialsService.getCredentialsDetails(new HashMap<>());
 
         // THEN
-        assertEquals(ACCOUNT_NAME_VALUE, actual.getAccountName());
-        assertEquals(ACCOUNT_KEY_VALUE, actual.getAccountKey());
+        assertEquals(ACCOUNT_NAME_SECRET_VALUE, actual.getAccountName());
+        assertEquals(ACCOUNT_KEY_SECRET_VALUE, actual.getAccountKey());
         assertNull(actual.getSasToken());
         assertFalse(actual.getUseManagedIdentity());
         assertNotNull(actual.getEndpointSuffix());
@@ -171,6 +180,7 @@ public class TestADLSCredentialsControllerService {
         // GIVEN
         configureAccountName();
         configureSasToken();
+        configureKeyVaultConnectionService();
 
         runner.enableControllerService(credentialsService);
 
@@ -178,29 +188,10 @@ public class TestADLSCredentialsControllerService {
         ADLSCredentialsDetails actual = credentialsService.getCredentialsDetails(new HashMap<>());
 
         // THEN
-        assertEquals(ACCOUNT_NAME_VALUE, actual.getAccountName());
-        assertEquals(SAS_TOKEN_VALUE, actual.getSasToken());
+        assertEquals(ACCOUNT_NAME_SECRET_VALUE, actual.getAccountName());
+        assertEquals(SAS_TOKEN_SECRET_VALUE, actual.getSasToken());
         assertNull(actual.getAccountKey());
         assertFalse(actual.getUseManagedIdentity());
-        assertNotNull(actual.getEndpointSuffix());
-    }
-
-    @Test
-    public void testGetCredentialsDetailsWithUseManagedIdentity() throws Exception {
-        // GIVEN
-        configureAccountName();
-        configureUseManagedIdentity();
-
-        runner.enableControllerService(credentialsService);
-
-        // WHEN
-        ADLSCredentialsDetails actual = credentialsService.getCredentialsDetails(new HashMap<>());
-
-        // THEN
-        assertEquals(ACCOUNT_NAME_VALUE, actual.getAccountName());
-        assertTrue(actual.getUseManagedIdentity());
-        assertNull(actual.getAccountKey());
-        assertNull(actual.getSasToken());
         assertNotNull(actual.getEndpointSuffix());
     }
 
@@ -210,6 +201,7 @@ public class TestADLSCredentialsControllerService {
         configureAccountName();
         configureAccountKey();
         configureEndpointSuffix();
+        configureKeyVaultConnectionService();
 
         runner.enableControllerService(credentialsService);
 
@@ -217,26 +209,30 @@ public class TestADLSCredentialsControllerService {
         ADLSCredentialsDetails actual = credentialsService.getCredentialsDetails(new HashMap<>());
 
         // THEN
-        assertEquals(END_POINT_SUFFIX_VALUE, actual.getEndpointSuffix());
+        assertEquals(END_POINT_SUFFIX_SECRET, actual.getEndpointSuffix());
     }
 
     private void configureAccountName() {
-        runner.setProperty(credentialsService, ADLSCredentialsControllerService.ACCOUNT_NAME, ACCOUNT_NAME_VALUE);
+        runner.setProperty(credentialsService, AzureStorageUtils.ACCOUNT_NAME_SECRET, ACCOUNT_NAME_SECRET);
     }
 
     private void configureAccountKey() {
-        runner.setProperty(credentialsService, AzureStorageUtils.ACCOUNT_KEY, ACCOUNT_KEY_VALUE);
+        runner.setProperty(credentialsService, AzureStorageUtils.ACCOUNT_KEY_SECRET, ACCOUNT_KEY_SECRET);
     }
 
     private void configureSasToken() {
-        runner.setProperty(credentialsService, AzureStorageUtils.PROP_SAS_TOKEN, SAS_TOKEN_VALUE);
-    }
-
-    private void configureUseManagedIdentity() {
-        runner.setProperty(credentialsService, ADLSCredentialsControllerService.USE_MANAGED_IDENTITY, "true");
+        runner.setProperty(credentialsService, AzureStorageUtils.ACCOUNT_SAS_TOKEN_SECRET, SAS_TOKEN_SECRET);
     }
 
     private void configureEndpointSuffix() {
-        runner.setProperty(credentialsService, AzureStorageUtils.ADLS_ENDPOINT_SUFFIX, END_POINT_SUFFIX_VALUE);
+        runner.setProperty(credentialsService, AzureStorageUtils.ADLS_ENDPOINT_SUFFIX, END_POINT_SUFFIX_SECRET);
+    }
+
+    private void configureKeyVaultConnectionService() throws InitializationException {
+        AzureKeyVaultClientService keyVaultConnectionService = new MockKeyVaultConnectionService();
+        String keyVaultServiceName = "keyvault-service";
+        runner.addControllerService(keyVaultServiceName, keyVaultConnectionService);
+        runner.setProperty(credentialsService, AzureStorageUtils.KEYVAULT_CONNECTION_SERVICE, keyVaultServiceName);
     }
 }
+
